6b122dc84a5bd161b18d407108e3e8b25ffffa8d
Dan1el131 <151350788+Dan1el131@users.noreply.github.com>
2026-06-30 13:26:18 +0100

    Delete GRAPPLE directory

--- GRAPPLE/grapple/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ["trainer", "models", "augment", "losses", "regularization"]
-"""GRAPPLE graph representation learning package."""
-
-from .models import GrappleModel, ModelConfig
-
-__all__ = ["GrappleModel", "ModelConfig"]
--- GRAPPLE/grapple/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .grapple import GrappleModel, ModelConfig
-from .prompting import PromptingConfig
-__all__ = ["GrappleModel", "ModelConfig", "PromptingConfig"]
