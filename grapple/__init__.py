e172c795779a7a079b508c6fcb3b83f88dbe917a
Dan1el131 <151350788+Dan1el131@users.noreply.github.com>
2026-06-30 13:26:42 +0100

    Add files via upload

---
+++ grapple/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["trainer", "models", "augment", "losses", "regularization"]
+"""GRAPPLE graph representation learning package."""
+
+from .models import GrappleModel, ModelConfig
+
+__all__ = ["GrappleModel", "ModelConfig"]
---
+++ grapple/models/__init__.py
@@ -0,0 +1,3 @@
+from .grapple import GrappleModel, ModelConfig
+
+__all__ = ["GrappleModel", "ModelConfig"]
